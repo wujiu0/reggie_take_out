6991564ec346232bab892455f604f2086f297bdc
wujiu <1050705928@qq.com>
2022-06-18 23:50:35 +0800

    init

---
+++ src/main/java/org/example/reggie/ReggieApplication.java
@@ -0,0 +1,14 @@
+package org.example.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class);
+        log.info("项目启动成功");
+    }
+}
