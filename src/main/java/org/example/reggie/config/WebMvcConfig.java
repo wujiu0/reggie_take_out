abc900fe890e6024afaeb887fa0f61a03127fde0
wujiu <1050705928@qq.com>
2022-06-19 23:54:12 +0800

    6/19 完成后台登录功能

---
+++ src/main/java/org/example/reggie/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package org.example.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
---
+++ src/main/java/org/example/reggie/controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package org.example.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.example.reggie.common.R;
+import org.example.reggie.entity.Employee;
+import org.example.reggie.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+
+//        1.将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+//        2.根据页面提交的用户名username查询数据库
+
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+//        3.如果没有查询到则返回登陆失败结果
+        if (emp == null) {
+            return R.error("登陆失败");
+        }
+
+//        4. 密码比对，如果不一致则返回登陆失败结果
+        if (!emp.getPassword().equals(password)) {
+            return R.error("登录失败");
+        }
+
+//        5.查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if (emp.getStatus() == 0) {
+            return R.error("账号已禁用");
+        }
+
+//        6.登录成功，将用户id存入Session并返回登陆成功结果
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+}
