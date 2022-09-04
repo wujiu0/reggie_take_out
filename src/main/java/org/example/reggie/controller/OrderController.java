fc1b01e1428b86a4b81e675126d509a9125b9738
wujiu <1050705928@qq.com>
2022-09-05 00:48:03 +0800

    V1.0 Release

--- src/main/java/org/example/reggie/controller/OrderController.java
+++ src/main/java/org/example/reggie/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.example.reggie.common.R;
 import org.example.reggie.entity.Orders;
 import org.example.reggie.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,7 +27,7 @@ public class OrderController {
     }
 
     @GetMapping("/userPage")
-    public R<Page<Orders>> page(int page, int pageSize) {
+    public R<Page<Orders>> userPage(int page, int pageSize) {
         Page<Orders> ordersPage = new Page<>(page, pageSize);
 
         LambdaQueryWrapper<Orders> ordersLambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -34,5 +35,23 @@ public class OrderController {
         orderService.page(ordersPage, ordersLambdaQueryWrapper);
 
         return R.success(ordersPage);
+    }
+
+    @GetMapping("/page")
+    public R<Page<Orders>> page(Integer page, Integer pageSize, String number, String beginTime, String endTime) {
+        log.info("订单查询：订单号:{},{}->{}", number, beginTime, endTime);
+        Page<Orders> ordersPage = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Orders> ordersLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        ordersLambdaQueryWrapper.eq(number != null, Orders::getNumber, number)
+                .between(StringUtils.hasText(beginTime), Orders::getOrderTime, beginTime, endTime);
+        orderService.page(ordersPage, ordersLambdaQueryWrapper);
+        return R.success(ordersPage);
+    }
+
+    @PutMapping
+    public R<String> update(@RequestBody Orders order) {
+        orderService.updateById(order);
+
+        return R.success("修改成功");
     }
 }
